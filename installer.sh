b04fee89d53698eb51e9410a80d2b81fe4bc5dc1
aqen <37166018+aqen@users.noreply.github.com>
2022-10-20 06:40:01 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
- sudo cp zerotier-cli zerotier-idtool -t /usr/local/bin/ && sudo cp zerotier-one /usr/sbin/ && sudo cp zerotier-one.service /etc/systemd/system/
- LINE=`sed -n -e "/\[Install\]/=" /etc/systemd/system/sshd.service`
- sed -i "$LINE a Wants=zerotier-one.service" /etc/systemd/system/sshd.service
+sudo chmod +x ./zerotier-* && sudo cp -p zerotier-cli zerotier-idtool -t /usr/local/bin/ && sudo cp -p zerotier-one /usr/sbin/ && sudo cp zerotier-one.service /etc/systemd/system/
+sudo LINE=`sed -n -e "/\[Install\]/=" /etc/systemd/system/sshd.service`
+sudo sed -i "$LINE a Wants=zerotier-one.service" /etc/systemd/system/sshd.service
+sudo systemctl daemon-reload && sudo systemctl enable zerotier-one && sudo systemctl start zerotier-one
+
